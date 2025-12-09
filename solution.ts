1ca2d33a7e5e6573c1583325405bd07287f15888
yeasinarafat51 <aloondokar65@gmail.com>
2025-12-09 13:29:30 +0600

    problem8

--- solution.ts
+++ solution.ts
@@ -20,3 +20,122 @@ function getLength(value: string | any[]): number {
     }
     return 0;
 }
+
+
+
+
+
+//   problem3
+
+
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
+
+
+
+// problem4
+
+interface Item {
+    title: string;
+    rating: number;
+}
+
+function filterByRating(items: Item[]): Item[] {
+    return items.filter(item => item.rating >= 4);
+}
+
+
+// problem5
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+
+function filterActiveUsers(users: User[]): User[] {
+    return users.filter(user => user.isActive === true);
+}
+
+// problem6
+
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): string {
+    return `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${book.isAvailable ? "Yes" : "No"}`;
+}
+
+// problem7
+
+function getUniqueValues(
+    arr1: (number | string)[],
+    arr2: (number | string)[]
+  ): (number | string)[] {
+  
+    const result: (number | string)[] = [];
+  
+    function addIfNotExists(value: number | string) {
+     
+      for (let i = 0; i < result.length; i++) {
+        if (result[i] === value) {
+          return; 
+        }
+      }
+      result[result.length] = value; 
+    }
+  
+   
+    for (let i = 0; i < array1.length; i++) {
+      addIfNotExists(array1[i]);
+    }
+  
+   
+    for (let i = 0; i < array2.length; i++) {
+      addIfNotExists(array2[i]);
+    }
+  
+    return result;
+  }
+  
+//   problem8
+
+  interface Product {
+    name: string;
+    price: number;
+    quantity: number;
+    discount?: number;
+}
+
+function calculateTotalPrice(products: Product[]): number {
+    if (products.length === 0) return 0;
+
+    return products
+        .map(product => {
+            const totalPrice = product.price * product.quantity;
+
+            if (product.discount) {
+                return totalPrice - (totalPrice * (product.discount / 100));
+            }
+
+            return totalPrice;
+        })
+        .reduce((sum, current) => sum + current, 0);
+}
+
